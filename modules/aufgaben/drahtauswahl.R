95a900d3ee79d00f88c5dc6d623ae457b24147d1
Andre Sers <andresers@MacBook-Pro-von-Andre.local>
2024-10-01 12:47:43 +0200

    zusammenfügen aller Ausfallpunkte

--- modules/aufgaben/drahtauswahl.R
+++ modules/aufgaben/drahtauswahl.R
@@ -5,20 +5,27 @@ drahtauswahl_ui <- function(id) {
     data_selector_ui(
       id = ns("id_data_selector")
     ),
-    actionButtonQW(
-      inputId = ns("add_histogram"),
-      label = NULL,
-      icon = icon("chart-area"),
-      tooltip = "Öffne Histogramm"
-    ),
+    #actionButtonQW(
+    #  inputId = ns("add_histogram"),
+    #  label = NULL,
+    #  icon = icon("chart-area"),
+    #  tooltip = "Öffne Histogramm"
+    #),
     actionButtonQW(
       inputId = ns("add_weibullgerade"),
       label = NULL,
       icon = icon("chart-area"),
       tooltip = "Weibullgerade anzeigen"
-    )
+    ),
+    dataTableOutput(ns("probability_table")),
+    dataTableOutput(ns("summary_table")),
+    dataTableOutput(ns("param_table")),
+    checkboxInput(ns("show_data"), "Auswertungsdaten anzeigen", FALSE),
+    checkboxInput(ns("show_params"), "Weibullparameter anzeigen", FALSE)
+    
   )
 }
+# Funktion zur Aufbereitung der Rohdaten
 refine_data <- function(raw_data, material) {
   refined_data <- raw_data[raw_data$Material == material, ] %>%
     select(c('Biegungsanzahl', 'Status', 'Material'))
@@ -30,6 +37,7 @@ refine_data <- function(raw_data, material) {
   
   return (rel_data)
 }
+# Funktion zur Beerchnung der weibull_parameter mittels Rangregression
 compute_weibull_params <- function(rel_data, model) {
   median_rank_data <- estimate_cdf(
     x = rel_data,
@@ -42,6 +50,7 @@ compute_weibull_params <- function(rel_data, model) {
   )
   return (weibull_params)
 }
+# Hilfsfunktion zum Hinzufügen weiterer Punkte in Weibullplot
 generate_probability_plot <- function(
     plot,
     rel_data,
@@ -100,55 +109,44 @@ drahtauswahl_server <- function(id, .values) {
         }
       })
       
-      ## Erstellen des Weibullplots für alle Materialien im geladenen Datensatz
+      # Erstellen des Weibullplots für alle Materialien im geladenen Datensatz
       observeEvent(input$add_weibullgerade, {
         drahtauswahl_raw <- data_r()
         materials <- unique(drahtauswahl_raw$Material)
-        plot_data <- dict()
+        plot_data <- data.frame()
         params <- dict()
         est_list <- list()
         names <- list()
         sample <- materials[1]
         
+        # Parameterschätzung und Rangregression für alle gegebenen Materialien
         for (material in materials) {
           refined_data <- refine_data(drahtauswahl_raw, material)
           weibull_params <- compute_weibull_params(refined_data, "mr")
-          plot_data[[material]] <- refined_data
+          median_data <- estimate_cdf(
+            x = refined_data,
+            methods = "mr"
+          )
+          plot_data <- rbind(plot_data, median_data)
           params[[material]] <- weibull_params
           est_list[[material]] <- weibull_params
         }
-        ## create basis plotly plot
+        # Erstellen des Basis-Weibullplots
         class(est_list) <- c("wt_model_estimation_list")
-        median_rank <- estimate_cdf(
-          x = plot_data[[sample]],
-          methods = "mr"
-        )
         base_plot <- plot_prob(
-          x = median_rank,
+          x = plot_data,
           distribution = "weibull",
           title_main = "Weibull - Wahrscheinlichkeitsnetz",
           title_x = "Biegungsanzahl",
           title_y = "Ausfallwahrscheinlichkeit in %",
-          title_trace = "Ausfälle Al",
+          title_trace = "Ausfälle",
           plot_method = "plotly"
         ) %>%
           plot_mod(
-            x = est_list
-          ) %>%
-          layout(yaxis2 = list(overlaying = "y",
-                               showgrid = FALSE,
-                               side = "right",
-                               showline = FALSE,
-                               zeroline = FALSE,
-                               showticklabels = FALSE))
-        for (i in 2:length(materials)) {
-          material <- materials[i]
-          base_plot <- generate_probability_plot(base_plot,
-                                                 plot_data[[material]],
-                                                 "mr",
-                                                 material
-                                                 )
-        }
+            x = est_list,
+            title_trace = "Ausgleichsgerade"
+          )
+          # Einfügen einer Überlagerten y-Achse für korrektes alignment aller Punkte
         plot_output_id <- paste0("weibullplot_", data_selector_return$name_r())
         .values$viewer$append_tab(
           tab = tabPanel(
@@ -159,10 +157,84 @@ drahtauswahl_server <- function(id, .values) {
             )
           )
         )
+        # Rendering des erstellten Weibullplots
         if (!hasName(output, plot_output_id)) {
           output[[plot_output_id]] <- renderPlotly({
             base_plot
           })
+        }
+      })
+      # Rendering der Auswertungstablle mit Wahrscheinlichkeitsschätzungen
+      output$summary_table <- renderDataTable({
+        
+        if(input$show_data){
+          drahtauswahl_raw <- data_r()
+          materials <- unique(drahtauswahl_raw$Material)
+          plot_data <- dict()
+          params <- dict()
+          sample <- materials[1]
+        
+          for (material in materials){
+            refined_data <- refine_data(drahtauswahl_raw, material)
+            prob_data <- estimate_cdf(refined_data, methods = "mr")
+            weibull_params <- compute_weibull_params(refined_data, "mr")
+            plot_data[[material]] <- prob_data
+            params[[material]] <- weibull_params
+          }
+          sample_frame <- data.frame(Material = sample, plot_data = plot_data[[sample]])
+          
+          for (i in 2:length(materials)) {
+            material <- materials[i]
+            new_frame <- data.frame(Material = material, plot_data = plot_data[[material]])
+            sample_frame <- rbind(sample_frame, new_frame)
+          }
+          print(sample_frame)
+          datatable(sample_frame)
+        }
+      })
+      # Rendering der Tabelle mit Weibullparametern und median Lebensdauern
+      output$param_table <- renderDataTable({
+        if (input$show_params){
+          drahtauswahl_raw <- data_r()
+          materials <- unique(drahtauswahl_raw$Material)
+          params <- dict()
+          sample <- materials[1]
+          
+          for (material in materials){
+            refined_data <- refine_data(drahtauswahl_raw, material)
+            prob_data <- estimate_cdf(refined_data, methods = "mr")
+            weibull_params <- compute_weibull_params(refined_data, "mr")
+            params[[material]] <- weibull_params
+          }
+          sample_t <- params[[sample]]$shape_scale_coefficients[1]
+          sample_b <- params[[sample]]$shape_scale_coefficients[2]
+          sample_lifetime <- predict_quantile(
+            p = 0.5, 
+            dist_params = params[[sample]]$coefficients,
+            distribution = "weibull"
+          )
+          sample_params <- data.frame(Material = sample,
+                                      param_T = sample_t,
+                                      param_b = sample_b,
+                                      median_lifetime = sample_lifetime)
+          
+          for (i in 2:length(materials)) {
+            material <- materials[i]
+            param_t <- params[[material]]$shape_scale_coefficients[1]
+            param_b <- params[[material]]$shape_scale_coefficients[2]
+            lifetime <- predict_quantile(
+              p = 0.5,
+              dist_params = params[[material]]$coefficients,
+              distribution = "weibull"
+            )
+            
+            new_params <- data.frame(Material = material,
+                                     param_T = param_t,
+                                     param_b = param_b,
+                                     median_lifetime = lifetime)
+            sample_params <- rbind(sample_params, new_params)
+          }
+          datatable(sample_params)
         }
       })
       
